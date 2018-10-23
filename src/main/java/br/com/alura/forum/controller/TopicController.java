a47b214521a61bce3ac95426a98b4dd985dad9df
spring7868 <spring7868@caelum144-02>
2018-10-23 20:20:17 -0300

    first class

---
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
+import br.com.alura.forum.controller.repository.TopicRepository;
+import br.com.alura.forum.model.topic_domain.Topic;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	@GetMapping(value = "api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<TopicBriefOutputDto> listTopics()
+	{
+		 
+		List<Topic> topics = topicRepository.list();
+		
+		return TopicBriefOutputDto.listFromTopics(topics);
+		
+	}
+
+}
---
+++ src/main/java/br/com/alura/forum/controller/dto/output/TopicBriefOutputDto.java
@@ -0,0 +1,124 @@
+package br.com.alura.forum.controller.dto.output;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.model.topic_domain.Topic;
+import br.com.alura.forum.model.topic_domain.TopicStatus;
+
+public	class	TopicBriefOutputDto	{
+	private	Long	id;
+	private	String	shortDescription;
+	private	long	secondsSinceLastUpdate;
+	private	String	ownerName;
+	private	String	courseName;
+	private	String	subcategoryName;
+	private	String	categoryName;
+	private	int	    numberOfResponses;
+	private	boolean	solved;
+	
+	public TopicBriefOutputDto(){
+		
+	}
+	
+	public	TopicBriefOutputDto(Topic	topic)	{
+		this.id	=	topic.getId().longValue();
+		this.shortDescription	=	topic.getShortDescription();
+		this.secondsSinceLastUpdate	=	getSecondsSince(topic.getLastUpdate());
+		this.ownerName	=	topic.getOwner().getName();
+		this.courseName	=	topic.getCourse().getName();
+		this.subcategoryName	=	topic.getCourse().getSubcategory().getName();
+		this.categoryName	=	topic.getCourse().getCategoryName();
+		this.numberOfResponses	=	topic.getNumberOfAnswers();
+		this.solved	=	TopicStatus.SOLVED.equals(topic.getStatus());
+	}
+	
+	private	long	getSecondsSince(Instant	lastUpdate)	{
+		
+		return	Duration.between(lastUpdate, Instant.now()).get(ChronoUnit.SECONDS);
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public long getSecondsSinceLastUpdate() {
+		return secondsSinceLastUpdate;
+	}
+
+	public void setSecondsSinceLastUpdate(long secondsSinceLastUpdate) {
+		this.secondsSinceLastUpdate = secondsSinceLastUpdate;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getSubcategoryName() {
+		return subcategoryName;
+	}
+
+	public void setSubcategoryName(String subcategoryName) {
+		this.subcategoryName = subcategoryName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public int getNumberOfResponses() {
+		return numberOfResponses;
+	}
+
+	public void setNumberOfResponses(int numberOfResponses) {
+		this.numberOfResponses = numberOfResponses;
+	}
+
+	public boolean isSolved() {
+		return solved;
+	}
+
+	public void setSolved(boolean solved) {
+		this.solved = solved;
+	}
+
+	public	static	List<TopicBriefOutputDto>	listFromTopics(List<Topic>	topics)	{
+		return	topics.stream()
+					.map(TopicBriefOutputDto::new)
+					.collect(Collectors.toList());
+	}	
+	
+	
+}
